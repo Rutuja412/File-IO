d5c219ce702d5dbd9b530cf8ac4c41b2f1fb10a6
Rutuja412 <rutujakadam4545@gmail.com>
2023-04-09 09:37:05 +0530

    [Rutuja]:Welcome

---
+++ File-IO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To File IO Project");
+        }
+    }
+}
